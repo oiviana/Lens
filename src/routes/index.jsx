4b0ff2f97eba15c6488845198e3323364b4c2a27
oiviana <vianalucas80@gmail.com>
2022-02-17 20:10:05 -0300

    restructuring routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,68 +1,23 @@
 import React from "react";
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import HomeNavigator from "./HomeNavigator";
-import NotificationNavigator from "./NotificationNavigator";
-import UserNavigator from "./UserNavigator";
-import LoginScreen from "../views/Login";
-import VacancyNavigator from "./VacancyNavigator";
-import {Entypo, Feather} from '@expo/vector-icons';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import LoginNavigator from './LoginNavigator';
+import Main from './Main';
 
+const Stack = createStackNavigator();
 
-const Tab = createBottomTabNavigator();
-
-export default function Routes(){
+export default function Routes() {
     return(
-        <Tab.Navigator
-        screenOptions={{
-            tabBarStyle:{
-                height: 55,
-                backgroundColor: '#EDEEF8',
-                paddingBottom:5,
-                paddingTop:5,    
-            },
-            tabBarActiveTintColor: 'black',
-            tabBarInactiveTintColor: '#616163',
-        }}
-
-        >
-            <Tab.Screen 
-            name="Home" 
-            component={HomeNavigator}
-            options={{
-                tabBarIcon:({size, color}) => (
-                    <Feather name="grid" size={size} color={color}/>
-                )
-            }}
-            />
-
-            <Tab.Screen 
-            name="Vagas" 
-            component={VacancyNavigator}
-            options={{
-                tabBarIcon:({size, color}) => (
-                    <Feather name="briefcase" size={size} color={color}/>
-                )
-            }}
-            />
-
-            <Tab.Screen 
-            name="Notificações" 
-            component={NotificationNavigator}
-            options={{
-                tabBarIcon:({size, color}) => (
-                    <Feather name="bell" size={size} color={color}/>
-                )
-            }}
-            />
-
-            <Tab.Screen name="Perfil"
-             component={UserNavigator}
-             options={{
-                tabBarIcon:({size, color}) => (
-                    <Feather name="user" size={size} color={color}/>
-                )
-            }}
-            />
-        </Tab.Navigator>
-    )
+    
+            <Stack.Navigator>
+                <Stack.Screen name="LoginScreen" component={LoginNavigator} options={{
+                       headerShown: false
+                }}/>
+                                <Stack.Screen name="MainScreen" component={Main} options = {{
+                    headerShown: false
+                }} />
+            </Stack.Navigator>
+  
+        
+    );
 }
